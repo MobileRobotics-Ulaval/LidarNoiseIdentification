79479d1567f9dca893415d7b072e54f911bf2e77
olfal1 <falolivier@hotmail.com>
2014-05-23 14:09:23 -0400

    initial commit

---
+++ Matlab/functions/plateFrameTransform.m
@@ -0,0 +1,20 @@
+function [T2] = plateFrameTransform(x2, y2, z2)
+% Creating transformation matrix to express points in the reference of the plate
+
+p0 = [x2(1,1), y2(1,1), z2(1,1)]; % bas
+p1 = [x2(2,1), y2(2,1), z2(2,1)]; % droite
+p2 = [x2(3,1), y2(3,1), z2(3,1)]; % haut
+
+center = -p2; % (0.0.0) en haut a gauche de la plaque
+nx = p1-p2;
+nz = cross(nx, p0-p2);
+ny = cross(nz, nx);
+
+
+R = [nx', ny', nz'];
+center = R\center'; % inv(R)*center pour tourner aussi le point de référence
+T2 = [inv(R), center];
+T2 = [T2; 0 0 0 1];
+
+end
+
