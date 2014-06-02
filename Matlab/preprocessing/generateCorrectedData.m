4eae36a15d7c28f4200c7939190769718e72a9d1
olfal1 <falolivier@hotmail.com>
2014-06-02 11:08:25 -0400

    progression bar of data generation added

--- Matlab/preprocessing/generateCorrectedData.m
+++ Matlab/preprocessing/generateCorrectedData.m
@@ -9,7 +9,7 @@ sensor = {'utm'};
 location = {'indoor'};
 plate = {'board', 'alu', 'steel', 'iron'};
 dist = {'d0_2', 'd1_0', 'd2_0', 'd4_0', 'd8_0'};
-
+counter = 0;
 for i = 1:length(sensor)
     for j = 1:length(location)
         for k = 1:length(plate)
@@ -18,12 +18,17 @@ for i = 1:length(sensor)
                    && isfield(data.(sensor{i}).(location{j}).(plate{k}), dist{m})
                
                    data = generatePoses_distance(data, sensor{i}, location{j}, plate{k}, dist{m}, false, true, .2, false);
+                   counter = counter + 1;
+                   disp(counter)
+                   fprintf('%s %s %s %s \n',sensor{i}, location{j}, plate{k}, dist{m})
                    
                 end
             end
         end
     end
 end
+
+disp('finished')
 
 %% Generate corrected data from angle dataset 
 
@@ -34,6 +39,7 @@ sensor = {'utm'};
 location = {'indoor'};
 plate = {'board', 'alu', 'steel', 'iron'};
 angle = {'a0', 'a15', 'a30', 'a45', 'a60', 'a75'};
+counter = 0;
 
 for i = 1:length(sensor)
     for j = 1:length(location)
@@ -41,8 +47,13 @@ for i = 1:length(sensor)
             for m = 1:length(angle)
                 
                    data = generatePoses_angle(data, sensor{i}, location{j}, plate{k}, angle{m}, false, true, .2, false);
-             
+                   counter = counter + 1;
+                   fprintf('%d/%d \n',counter, length(angle)*length(plate)*length(location)*length(sensor));
+
             end
         end
     end
 end
+disp('finished')
+
+clear sensor location plate angle i j k m counter
--- Matlab/preprocessing/generateData.m
+++ Matlab/preprocessing/generateData.m
@@ -5,21 +5,26 @@ sensor = {'urg','utm','lms'};
 location = {'indoor','outdoor'};
 plate = {'board', 'alu', 'steel', 'iron'};
 dist = {'d0_2', 'd1_0', 'd2_0', 'd4_0', 'd8_0'};
-
+counter = 0;
 for i = 1:length(sensor)
     for j = 1:length(location)
         for k = 1:length(plate)
             for m = 1:length(dist)
                 if isfield(data.(sensor{i}), location{j}) ...
                    && isfield(data.(sensor{i}).(location{j}).(plate{k}), dist{m})
-               
+                    
+                   
                    data = generatePoses_distance(data, sensor{i}, location{j}, plate{k}, dist{m}, false, false, .2, false);
+                   counter = counter + 1;
+                   disp(counter)
+                   fprintf('%s %s %s %s \n',sensor{i}, location{j}, plate{k}, dist{m})
                    
                 end
             end
         end
     end
 end
+disp('finished')
 
 %% Generate data from angle dataset 
 % *** generateIDs_angle before ***
@@ -28,15 +33,19 @@ sensor = {'utm','lms'};
 location = {'indoor'};
 plate = {'board', 'alu', 'steel', 'iron'};
 angle = {'a0', 'a15', 'a30', 'a45', 'a60', 'a75'};
+counter = 0;
 
 for i = 1:length(sensor)
     for j = 1:length(location)
         for k = 1:length(plate)
             for m = 1:length(angle)
-                
+                    
                    data = generatePoses_angle(data, sensor{i}, location{j}, plate{k}, angle{m}, false, false, .2, false);
-             
+                   counter = counter + 1;
+                   fprintf('%d/%d \n',counter, length(angle)*length(plate)*length(location)*length(sensor));
             end
         end
     end
 end
+
+clear sensor location plate angle i j k m counter
